ca431dee7a4fd23a35d2b40d107162bd9e1fb82a
mickyco94 <michael.clerkin-oliver@pushdr.com>
2020-10-03 20:33:31 +0100

    feat: add api versioning, swagger + serilog

--- src/Checkout.Gateway.API/Startup.cs
+++ src/Checkout.Gateway.API/Startup.cs
@@ -1,8 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MockBank.API.Client;
+using Serilog;
+using System;
 
 namespace Checkout.Gateway.API
 {
@@ -18,11 +22,32 @@ namespace Checkout.Gateway.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMockBankApiClient(o =>
+            {
+                o.BaseAddress = new Uri(Configuration["MockBankApiUrl"]);
+            });
+
+            services.AddSwaggerGen();
+
+            services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+            services.AddApiVersioning(options =>
+            {
+                options.ReportApiVersions = true;
+            });
+
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(
+            IApplicationBuilder app,
+            IWebHostEnvironment env,
+            IApiVersionDescriptionProvider apiVersionDescriptionProvider)
         {
             if (env.IsDevelopment())
             {
@@ -34,6 +59,18 @@ namespace Checkout.Gateway.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+                foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions)
+                {
+                    options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                }
+            });
+
+            app.UseSerilogRequestLogging();
 
             app.UseEndpoints(endpoints =>
             {
